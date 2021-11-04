e6c69757cb712e858482d713b8fbd0512366c8e1
Valentin Gartiser <valentin.gartiser@forst.bwl.de>
2021-11-04 17:07:22 +0100

    updated the way NextMethod() is called and added documentation for pf_efflux / pf_prod

--- R/proflux_extract.R
+++ R/proflux_extract.R
@@ -1,16 +1,9 @@
 #' @title proflux_extract
 #' @description Utility function for the fast extraction and aggregation of
 #' parameters exported by pro_flux
-#' @param PROFLUX The data.frame returned by pro_flux()
-#' @param target Character string defining the type of parameter to be
-#' extracted / aggregated.
-#' One of:
-#' \describe{
-#'   \item{efflux}{Extract the efflux at the atmosphere/soil interface.
-#' This is the flux at the topmost position.}
-#' \item{prod_layer}{Extract the total production per layer defined in the function call
-#' of pro_flux()}
-#' }
+#' @param PROFLUX The PFres object returned by pro_flux()
+#' @param id_cols The id_cols used in the function call(only needed
+#' if PROFLUX is not a PFres object).
 #' @return data.frame
 #' @export
 #'
@@ -21,47 +14,39 @@
   }
 
   #' @export
-  pf_efflux.PFres <- function(PROFLUX) {
-
+  pf_efflux.PFres <- function(PROFLUX,
+                              ...) {
     id_cols <- id_cols(PROFLUX)
+    NextMethod(id_cols = id_cols)
+  }
 
+  #' @export
+  pf_efflux.data.frame <- function(PROFLUX,
+                                   id_cols) {
     #extract the topmost flux per date per id_col
     efflux <- get_efflux(PROFLUX,
                          id_cols)
   }
 
   #' @export
-  pf_efflux.data.frame <- function(PROFLUX,
-                                   id_cols) {
-
-    #extract the topmost flux per date per id_col
-    efflux <- get_efflux(PROFLUX,
-                         id_cols)
-  }
-
   pf_prod <- function(PROFLUX,
                       ...) {
     UseMethod("pf_prod")
   }
 
   #' @export
-  pf_prod.PFres <- function(PROFLUX) {
-
+  pf_prod.PFres <- function(PROFLUX,
+                            ...) {
     id_cols <- id_cols(PROFLUX)
-
-    #extract the total production per layer
-    prod <- get_prod(PROFLUX,
-                     id_cols)
+    NextMethod(id_cols = id_cols)
   }
 
   #' @export
   pf_prod.data.frame <- function(PROFLUX,
                                  id_cols) {
-
     #extract the total production per layer
     prod <- get_prod(PROFLUX,
                      id_cols)
-
   }
 
   ####################
