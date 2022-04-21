486ee76a9b1a30420d8a74c27ae65bee0abe4341
Valentin Gartiser <valentin.gartiser@forst.bwl.de>
2022-04-21 18:44:01 +0200

    added a test that checks that alternate() does not change the model if no changes should occur

--- tests/testthat/test-alternate.R
+++ tests/testthat/test-alternate.R
@@ -45,3 +45,57 @@ test_that("topheight only", {
 
 
 })
+
+
+
+test_that("no change",{
+
+
+  soilphys <-
+    ConFluxPro::soilphys %>%
+    cfp_soilphys(id_cols = c("site", "Date"))
+
+  gasdata <-
+    ConFluxPro::gasdata %>%
+    cfp_gasdata(id_cols = c("site", "Date"))
+
+
+  lmap <- soilphys %>%
+    select(upper,site) %>%
+    distinct() %>%
+    group_by(site) %>%
+    slice_max(upper) %>%
+    summarise(upper = c(upper,0),
+              lower = c(0,-100)) %>%
+    cfp_layers_map(gas = "CO2",
+                   layer_couple = 0,
+                   lowlim = 0,
+                   highlim = 1000,
+                   id_cols = "site")
+  PROFLUX <-
+    cfp_dat(gasdata,
+            soilphys,
+            lmap ) %>%
+    pro_flux()
+
+
+  run_map <- run_map(PROFLUX,
+                     params = list("topheight" = c(0,1),
+                                   "TPS" = c(1,2)),
+                     method = "random",
+                     n_runs = 1,
+                     type = c("addition", "factor")
+  ) %>%
+    mutate(value = ifelse(type == "factor",1,0))
+
+  PF_nochange <-
+  alternate(PROFLUX,
+            run_map = run_map,
+            f = function(x) complete_soilphys(x, overwrite = TRUE),
+            return_raw = TRUE)[[1]]
+
+  expect_equal(efflux(PF_nochange), efflux(PROFLUX))
+
+
+
+})
