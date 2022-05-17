1bc6b6e351cc5f11b190adfe3be0a172f18663ad
Valentin Gartiser <valentin.gartiser@forst.bwl.de>
2022-05-17 17:14:03 +0200

    updated example in pro_flux()

--- R/pro_flux.R
+++ R/pro_flux.R
@@ -43,26 +43,35 @@
 #' defines the weight of this penalty in the optimisation algorithm \code{\link{prod_optim}}.
 #'
 #' @examples {
-#' data("gasdata")
-#' data("soilphys")
 #'
-#' library(dplyr)
+#'library(dplyr)
 #'
-#' lmap <- soilphys %>%
-#'   select(upper,site) %>%
-#'   distinct() %>%
-#'   group_by(site) %>%
-#'   slice_max(upper) %>%
-#'   summarise(upper = c(upper,0),
-#'             lower = c(0,-100),
-#'             lowlim = 0,
-#'             highlim = 1000,
-#'             layer_couple = 0)
-#' PROFLUX <-
-#'   pro_flux(gasdata,
-#'            soilphys,
-#'            lmap,
-#'            c("site","Date"))
+#'soilphys <-
+#'  ConFluxPro::soilphys %>%
+#'  cfp_soilphys(id_cols = c("site", "Date"))
+#'
+#'gasdata <-
+#'  ConFluxPro::gasdata %>%
+#'  cfp_gasdata(id_cols = c("site", "Date"))
+#'
+#'
+#'lmap <- soilphys %>%
+#'  select(upper,site) %>%
+#'  distinct() %>%
+#'  group_by(site) %>%
+#'  slice_max(upper) %>%
+#'  summarise(upper = c(upper,0),
+#'            lower = c(0,-100)) %>%
+#'  cfp_layers_map(gas = "CO2",
+#'                 layer_couple = 0,
+#'                 lowlim = 0,
+#'                 highlim = 1000,
+#'                 id_cols = "site")
+#'PROFLUX <-
+#'  cfp_dat(gasdata,
+#'          soilphys,
+#'          lmap ) %>%
+#'  pro_flux()
 #' }
 #'
 #' @family proflux
