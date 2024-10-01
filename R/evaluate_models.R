0413e90557b7cdc496e18d0060aac19d1e60507e
Valentin Gartiser <Valentin.Gartiser@forst.bwl.de>
2024-10-01 13:22:08 +0200

    finalized first draft of evaluation function

--- R/evaluate_models.R
+++ R/evaluate_models.R
@@ -10,7 +10,11 @@
 #' @export
 evaluate_models <- function(x,
                             eval_funs = NULL,
+                            eval_weights = 1,
                             param_cols,
+                            eval_cols,
+                            n_best = NULL,
+                            f_best = 0.01,
                             scaling_fun = scale_min_median){
   UseMethod("evaluate_models")
 }
@@ -20,7 +24,11 @@ evaluate_models <- function(x,
 #' @exportS3Method
 evaluate_models.cfp_altres <- function(x,
                                        eval_funs = NULL,
+                                       eval_weights = 1,
                                        param_cols = cfp_id_cols(cfp_layers_map(cfp_og_model(PF_alt))),
+                                       eval_cols,
+                                       n_best = NULL,
+                                       f_best = 0.01,
                                        scaling_fun = scale_min_median){
 
   stopifnot("Provide at least one eval_funs" = !is.null(eval_funs))
@@ -35,23 +43,50 @@ evaluate_models.cfp_altres <- function(x,
   #stopifnot("eval_args must be a list of the same length as eval_funs or NULL" =
   #            is.null(eval_args) | (is.list(eval_args) & (length(eval_args) == length(eval_funs))))
 
+  stopifnot("eval_weights must be of length 1 or of the same length as eval_funs" =
+              (length(eval_funs) == length(eval_weights)) | (length(eval_weights) == 1))
+
+  stopifnot("f_best must be a fraction between 0 and 1" = 0 <= f_best & f_best <= 1 )
+
+  weights_df <- data.frame(error_parameter = names(eval_funs),
+                           weight_parameter = eval_weights)
+
+  n_runs <- length(x)
+
+  if (is.null(n_best)){
+    n_best <- ceiling(n_runs * f_best)
+  }
 
   x_eval_funs_applied <-
     lapply(FUN = apply_eval_fun,
            X = eval_funs,
-           x = x) %>%
+           x = x,
+           param_cols = param_cols) %>%
     dplyr::bind_rows(.id = "error_parameter") %>%
     dplyr::group_by(dplyr::across(dplyr::all_of(c("error_parameter", param_cols)))) %>%
-    dplyr::mutate(sNRMSE = scaling_fun(NRMSE))
+    dplyr::mutate(sNRMSE = scaling_fun(NRMSE)) %>%
+    dplyr::left_join(weights_df, by = "error_parameter")
+
+  x_model_error <-
+    x_eval_funs_applied %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(c("run_id", eval_cols)))) %>%
+    dplyr::summarise(ME = sum(sNRMSE*weight_parameter)) %>%
+    dplyr::arrange(ME)
 
 
+  best_runs <- x_model_error %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(c(eval_cols)))) %>%
+    dplyr::slice_min(ME, n = n_best)
 
-  x_eval_funs_applied
+  list(best_runs = best_runs,
+       model_error = x_model_error,
+       x_evaluated = x_eval_funs_applied)
 }
 
 
 
 apply_eval_fun <- function(x,
+                           param_cols,
                            cur_eval_fun){
 
   # cur_eval_args <- c("x" = list(x),
@@ -62,7 +97,9 @@ apply_eval_fun <- function(x,
   #             all(names(cur_eval_args) %in% names((formals(cur_eval_fun)))))
 
   x_eval <-
-  do.call(cur_eval_fun, args = list(x=x))
+  do.call(cur_eval_fun,
+          args = list(x = x,
+                      param_cols = param_cols))
   x_eval
 }
 
