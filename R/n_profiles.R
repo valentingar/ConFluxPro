dcf193b293c28e98926e5adaa4366e17254b52ec
Valentin Gartiser <Valentin.Gartiser@forst.bwl.de>
2024-10-10 12:21:35 +0200

    Added main package and data_preparation vignettes

--- R/n_profiles.R
+++ R/n_profiles.R
@@ -11,16 +11,21 @@ n_profiles.cfp_dat <- function(x) {
 
 #' @exportS3Method
 n_profiles.cfp_soilphys <- function(x) {
-  id_cols_to_n_profiles(x)
+  NextMethod()
 }
 
 #' @exportS3Method
 n_profiles.cfp_gasdata <- function(x) {
-  id_cols_to_n_profiles(x)
+  NextMethod()
 }
 
 #' @exportS3Method
 n_profiles.cfp_layers_map <- function(x) {
+  NextMethod()
+}
+
+#' @exportS3Method
+n_profiles.cfp_profile <- function(x){
   id_cols_to_n_profiles(x)
 }
 
